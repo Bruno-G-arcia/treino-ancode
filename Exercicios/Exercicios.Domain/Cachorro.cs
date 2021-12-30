60f505707e7966014b1f9dbf520be7b04e72b630
Bruno-G-arcia <brunogarciaengcivil@gmail.com>
2021-12-30 09:22:26 -0300

    aula14

---
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Domain
+{
+    public class Cachorro
+    {
+        private string _nome    = "";
+        private string _sexo    = "";
+        private string _raca    = "";
+        private string _porte   = "";
+        private int    _idade;
+        private double _peso;
+
+
+        public string Latir()
+        {
+            return "Bork";
+        }
+        public string QuantoDevoComer(int pesoKg)
+        {
+            return $"Como tenho {pesoKg}kg, devo comer {pesoKg * 50}g por dia";
+        }
+
+        public void SetNome(string nome)
+        {
+            _nome = nome;
+        }
+
+        public object GetNome()
+        {
+            return _nome;
+        }
+    }
+}
---
+++ Exercicios/Exercicios.Tests/cachorroTest.cs
@@ -0,0 +1,35 @@
+﻿using Exercicios.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Exercicios.Tests
+{
+    [TestClass] 
+    public class cachorroTest
+    {
+        [TestMethod]
+        public void Cachorro_Latir_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string latido = leia.Latir();
+            Console.WriteLine(latido);
+            Assert.AreEqual("Bork", latido);
+        }
+        [TestMethod]
+        public void Cachorro_QuantoDevoComer_Test()
+        {
+            Cachorro leia = new Cachorro();
+            string quantoDevoComer = leia.QuantoDevoComer(1);
+            Console.WriteLine(quantoDevoComer);
+            Assert.AreEqual("Como tenho 1kg, devo comer 50g por dia", quantoDevoComer);
+        }
+        [TestMethod]
+        public void Cachorro_Set_Get_Nome_Teste()
+        {
+            Cachorro yuri = new Cachorro();
+            yuri.SetNome("Yuri");
+            Assert.AreEqual("Yuri", yuri.GetNome());
+            Console.WriteLine(yuri.GetNome());
+        }
+    }
+}
