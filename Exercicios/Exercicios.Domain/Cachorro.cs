abd0ffed744332d46b5ce9c0a609e46f023b998f
Bruno-G-arcia <brunogarciaengcivil@gmail.com>
2021-12-30 11:58:40 -0300

    auto-property

--- Exercicios/Exercicios.Domain/Cachorro.cs
+++ Exercicios/Exercicios.Domain/Cachorro.cs
@@ -1,38 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercicios.Domain
+﻿namespace Exercicios.Domain
 {
     public class Cachorro
     {
-        private string _nome    = "";
-        private string _sexo    = "";
-        private string _raca    = "";
-        private string _porte   = "";
-        private int    _idade;
-        private double _peso;
+        public string? Nome { get; set; } = "";
 
-
-        public string Latir()
+        public static string Latir()
         {
             return "Bork";
         }
-        public string QuantoDevoComer(int pesoKg)
+        public static string QuantoDevoComer(int pesoKg)
         {
             return $"Como tenho {pesoKg}kg, devo comer {pesoKg * 50}g por dia";
-        }
-
-        public void SetNome(string nome)
-        {
-            _nome = nome;
-        }
-
-        public object GetNome()
-        {
-            return _nome;
-        }
+        }        
     }
 }
--- Exercicios/Exercicios.Tests/cachorroTest.cs
+++ Exercicios/Exercicios.Tests/cachorroTest.cs
@@ -11,7 +11,7 @@ namespace Exercicios.Tests
         public void Cachorro_Latir_Test()
         {
             Cachorro leia = new Cachorro();
-            string latido = leia.Latir();
+            string latido = Cachorro.Latir();
             Console.WriteLine(latido);
             Assert.AreEqual("Bork", latido);
         }
@@ -19,7 +19,7 @@ namespace Exercicios.Tests
         public void Cachorro_QuantoDevoComer_Test()
         {
             Cachorro leia = new Cachorro();
-            string quantoDevoComer = leia.QuantoDevoComer(1);
+            string quantoDevoComer = Cachorro.QuantoDevoComer(1);
             Console.WriteLine(quantoDevoComer);
             Assert.AreEqual("Como tenho 1kg, devo comer 50g por dia", quantoDevoComer);
         }
@@ -27,9 +27,9 @@ namespace Exercicios.Tests
         public void Cachorro_Set_Get_Nome_Teste()
         {
             Cachorro yuri = new Cachorro();
-            yuri.SetNome("Yuri");
-            Assert.AreEqual("Yuri", yuri.GetNome());
-            Console.WriteLine(yuri.GetNome());
+            yuri.Nome = "Yuri";
+            Assert.AreEqual("Yuri", yuri.Nome);
+            Console.WriteLine(yuri.Nome);
         }
     }
 }
